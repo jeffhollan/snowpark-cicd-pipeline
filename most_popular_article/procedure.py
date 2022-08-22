dd7ddbd0eb41d59dfa8d0c6a3a320ae5c69b7907
Jeff Hollan <jeffhollan@outlook.com>
2022-08-21 21:51:22 -0700

    delete procedure.py

--- most_popular_article/procedure.py
+++
@@ -1,8 +0,0 @@
-from snowflake.snowpark import Session
-from shared.snowflake_connection import SnowflakeConnection
-import process_data
-
-
-def invoke(session: Session) -> str:
-    SnowflakeConnection().connection = session
-    return process_data.notebook(session)._show_string()
